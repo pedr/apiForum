221a0ab92c985717c43e97c015a05966af833fef
pedro l <pedrlz.frn@gmail.com>
2019-07-06 18:44:26 -0300

    Deixando o codigo mais bunito

--- db/Thread.js
+++ db/Thread.js
@@ -6,25 +6,25 @@ module.exports = class Thread {
 	constructor({ id, authorId, content, isVisible = true, posts = [] }) {
 		this.id = id;
 		this.authorId = authorId;
-        this.isVisible = isVisible;
-        this.content = content;
-        this.posts = posts;
+		this.isVisible = isVisible;
+		this.content = content;
+		this.posts = posts;
 	}
 
 	loadInfo(id = this.id) {
 		return new Promise((resolve, reject) => {
 			knex.select('author_id as authorId', 'is_visible as isVisible', 'content')
-			.from('threads')
-			.where('id', id)
-			.then(resp => {
-                const thread = resp[0];
-                Object.assign(this, thread);
-				resolve();
-			})
-			.catch(e => {
-				console.error(e);
-				reject();
-			})
+				.from('threads')
+				.where('id', id)
+				.then(resp => {
+					const thread = resp[0];
+					Object.assign(this, thread);
+					resolve();
+				})
+				.catch(e => {
+					console.error(e);
+					reject();
+				})
 		})
 	}
 
@@ -41,7 +41,7 @@ module.exports = class Thread {
 				.into('threads')
 				.returning(['id', 'author_id as authorId', 'is_visible as isVisible', 'content'])
 				.then(resp => {
-                    Object.assign(this, resp);
+					Object.assign(this, resp);
 					resolve();
 				})
 				.catch(e => {
@@ -49,24 +49,24 @@ module.exports = class Thread {
 					reject();
 				})
 		})
-    }
-    
-    getPosts() {
-        return new Promise((resolve, reject) => {
-            if (!this.id) {
-                reject();
-            }
-            knex.select('id', 'thread_id as threadId', 'content', 'user_id as userId')
-                .from('posts')
-                .where('thread_id', this.id)
-                .then(resp => {
-                    this.posts = resp.map(post => new Post(post));
-                    resolve();
-                })
-                .catch(e => {
-                    console.error(e);
-                    reject();
-                })
-        })
-    }
+	}
+
+	getPosts() {
+		return new Promise((resolve, reject) => {
+			if (!this.id) {
+				reject();
+			}
+			knex.select('id', 'thread_id as threadId', 'content', 'user_id as userId')
+				.from('posts')
+				.where('thread_id', this.id)
+				.then(resp => {
+					this.posts = resp.map(post => new Post(post));
+					resolve();
+				})
+				.catch(e => {
+					console.error(e);
+					reject();
+				})
+		})
+	}
 }
